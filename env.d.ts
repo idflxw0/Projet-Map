5c224a4b93a45508e0b5bdf4b55d275c367a3e7e
idfl0wx <idfl0wxzero@gmail.com>
2023-12-28 15:45:57 +0100

    map

---
+++ env.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: "AIzaSyBWre3X8G7M4O62TSKt2dOC2-1ggLb7pP0";
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "../components/map";
+
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBWre3X8G7M4O62TSKt2dOC2-1ggLb7pP0",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
